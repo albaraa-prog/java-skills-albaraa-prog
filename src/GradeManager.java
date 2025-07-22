29c0fc1935d217ea1961bdfd41cd9a0e3aaa0e42
albaraa-prog <albraa9021@gmail.com>
2025-07-22 15:26:37 +0400

    added Method to calculate letter grades

--- src/GradeManager.java
+++ src/GradeManager.java
@@ -9,4 +9,25 @@ public class GradeManager {
         }
         return names;
     }
+
+    // Method to calculate letter grades
+    public static char getLetterGrade(int score) {
+        char grade = 'A';
+        if(score >= 90 && score <= 100){
+            grade = 'A';
+        } else if(score >= 80 && score <= 89) {
+            grade = 'B';
+        } else if(score >= 70 && score <= 79){
+            grade = 'C';
+        } else if(score >= 60 && score <= 69) {
+            grade = 'D';
+        } else if(grade <= 60){
+            grade = 'F';
+        } else {
+            System.out.println("Invalid Input");
+        }
+// Use if-else conditions:
+// 90-100: 'A', 80-89: 'B', 70-79: 'C', 60-69: 'D', below 60: 'F'
+        return grade;
+    }
 }
