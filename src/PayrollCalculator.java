ff209a4d9369fe5973f9dab5240025f6872649a5
albaraa-prog <albraa9021@gmail.com>
2025-07-22 15:05:04 +0400

    updated Employee Payroll System class

--- src/PayrollCalculator.java
+++ src/PayrollCalculator.java
@@ -2,20 +2,13 @@ package src;
 
 import java.lang.classfile.instruction.SwitchCase;
 import java.util.Scanner;
+import java.util.WeakHashMap;
 
 public class PayrollCalculator {
-    // Method to calculate weekly pay based on employee type and hours
     public static double calculateWeeklyPay(String employeeType, double hoursWorked, double hourlyRate) {
-// Employee types: "FULL_TIME", "PART_TIME", "CONTRACTOR", "INTERN"
-// Use switch case for different rules:
-// FULL_TIME: Regular pay for 40 hours, overtime (1.5x) for hours > 40
-// PART_TIME: Regular pay, no overtime, max 25 hours
-// CONTRACTOR: Flat rate, no overtime rules
-// INTERN: 20% discount from hourly rate, max 20 hours
-// Handle invalid employee types and negative values
         int choice;
         double calculated = 0;
-
+        Boolean overtime = false;
         Scanner sc = new Scanner(System.in);
 
         do{
@@ -31,14 +24,55 @@ public class PayrollCalculator {
 
             switch(choice){
                 case 1:
+                    System.out.println("Enter working hours: ");
+                    hoursWorked = sc.nextDouble();
+                    System.out.println("Enter hourly rate: ");
+                    hourlyRate = sc.nextDouble();
+
+                    if (hoursWorked > 40){
+                        calculated = (hoursWorked * hourlyRate) * 1.5;
+                    }else {
+                        calculated = hoursWorked * hourlyRate;
+                    }
+                    System.out.println("Result: " + calculated);
                     break;
                 case 2:
+                    System.out.println("Enter working hours: ");
+                    hoursWorked = sc.nextDouble();
+                    if (hoursWorked >= 25){
+                        System.out.println("Max working hours is 25! Try again.");
+                        hoursWorked = sc.nextDouble();
+                    }
+                    System.out.println("Enter hourly rate: ");
+                    hourlyRate = sc.nextDouble();
+
+                    calculated = hoursWorked * hourlyRate;
+                    System.out.println("Result: " + calculated);
                     break;
                 case 3:
+                    System.out.println("Enter working hours: ");
+                    hoursWorked = sc.nextDouble();
+                    System.out.println("Enter hourly rate: ");
+                    hourlyRate = sc.nextDouble();
+                    calculated = hoursWorked * hourlyRate;
+                    System.out.println("Result: " + calculated);
                     break;
                 case 4:
+                    System.out.println("Enter working hours: ");
+                    hoursWorked = sc.nextDouble();
+
+                    if (hoursWorked >= 20){
+                        System.out.println("Max working hours is 20! Try again.");
+                        hoursWorked = sc.nextDouble();
+                    }
+                    System.out.println("Enter hourly rate: ");
+                    hourlyRate = sc.nextDouble();
+                    double dis = (hoursWorked * hourlyRate) - (20 / 100);
+                    calculated = (hoursWorked * hourlyRate) - dis;
+                    System.out.println("Result: " + calculated);
                     break;
                 case 5:
+                    System.out.println("Closing System...");
                     break;
                 default:
                     System.out.println("Invalid choice! Please enter a number between 1 and 5.");
