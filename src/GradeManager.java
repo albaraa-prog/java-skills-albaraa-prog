73fdd8679cf423bfcc0125dec03a49eacce56594
albaraa-prog <albraa9021@gmail.com>
2025-07-22 15:17:13 +0400

    added Method to reverse student names

--- src/GradeManager.java
+++ src/GradeManager.java
@@ -1,5 +1,12 @@
 package src;
 
 public class GradeManager {
-
+    // Method to reverse student names in an array
+    public static String[] reverseStudentNames(String[] names) {
+        for(int i = 0; i < names.length ; i++){
+            String mod = new StringBuilder(names[i]).reverse().toString();
+            names[i] = mod;
+        }
+        return names;
+    }
 }
